a9652922e05a717a52b7daebc3944785b4b1c20d
Heman <hemanbermudez269970@correo.itm.edu.co>
2026-05-14 08:31:37 -0500

    se agregar la opción de editar el plan de ahorro

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,37 @@
 <?php
-
+ 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PlanAhorroController;
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/', function () {
     return view('home');
 });
-
+ 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
-
+ 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/password', [ProfileController::class, 'update'])->name('password.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+ 
 Route::middleware('auth')->group(function () {
     Route::get('/salario', function () {
         return view('salario');
     })->name('salario');
-
-    Route::get('/ahorro', [PlanAhorroController::class, 'index'])->name('ahorro');
-    Route::post('/ahorro', [PlanAhorroController::class, 'store'])->name('ahorro.store');
-    Route::delete('/ahorro/{id}', [PlanAhorroController::class, 'destroy'])->name('ahorro.destroy');
-
+ 
+    Route::get('/ahorro',              [PlanAhorroController::class, 'index'])->name('ahorro');
+    Route::post('/ahorro',             [PlanAhorroController::class, 'store'])->name('ahorro.store');
+    Route::patch('/ahorro/{id}',       [PlanAhorroController::class, 'update'])->name('ahorro.update');
+    Route::delete('/ahorro/{id}',      [PlanAhorroController::class, 'destroy'])->name('ahorro.destroy');
+ 
     Route::get('/gastos', function () {
         return view('gastos');
     })->name('gastos');
 });
-
+ 
 require __DIR__.'/auth.php';
